01aeb62936b30dbe445ac2273787f1a953277763
Daniel Wright <dwrigh58@uncc.edu>
2016-12-05 01:11:09 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,4 @@
-# Default is World
-# Author: Daniel Wright (dwrigh58@uncc.edu)
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
+puts "Hello, #{my_name}!"
